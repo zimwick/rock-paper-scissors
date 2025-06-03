32d8c394d984e13b3bef2f6339faccd367c592b7
zimwick <121416169+zimwick@users.noreply.github.com>
2025-06-03 13:55:32 -0600

    Add game session logic
    created the initial game state and logic

--- script.js
+++ script.js
@@ -14,24 +14,53 @@ const getComputerChoice = function () {
 };
 
 const playRound = function (humanChoice, computerChoice) {
-  const humanChoiceLower = humanChoice.toLowerCase();
+  let computerImagePath;
+  let humanImagePath;
 
-  if (humanChoiceLower === "s" && computerChoice === "p") {
+  switch (humanChoice) {
+    case "s":
+      humanImagePath = "./imgs/Scissors.png";
+      break;
+    case "r":
+      humanImagePath = "./imgs/Rock.png";
+      break;
+    case "p":
+      humanImagePath = "./imgs/Paper.png";
+      break;
+  }
+  switch (computerChoice) {
+    case "s":
+      computerImagePath = "./imgs/Scissors.png";
+      break;
+    case "r":
+      computerImagePath = "./imgs/Rock.png";
+      break;
+    case "p":
+      computerImagePath = "./imgs/Paper.png";
+      break;
+  }
+
+  playerImage.setAttribute("src", humanImagePath);
+  computerImage.setAttribute("src", computerImagePath);
+  playerImage.style.visibility = "visible";
+  computerImage.style.visibility = "visible";
+
+  if (humanChoice === "s" && computerChoice === "p") {
     console.log("You win! Scissors beats Paper");
     return "player";
-  } else if (humanChoiceLower === "s" && computerChoice === "r") {
+  } else if (humanChoice === "s" && computerChoice === "r") {
     console.log("You lose! Rock beats Scissors");
     return "computer";
-  } else if (humanChoiceLower === "p" && computerChoice === "r") {
+  } else if (humanChoice === "p" && computerChoice === "r") {
     console.log("You win! Paper beats Rock");
     return "player";
-  } else if (humanChoiceLower === "p" && computerChoice === "s") {
+  } else if (humanChoice === "p" && computerChoice === "s") {
     console.log("You lose! Scissors beats Paper");
     return "computer";
-  } else if (humanChoiceLower === "r" && computerChoice === "s") {
+  } else if (humanChoice === "r" && computerChoice === "s") {
     console.log("You win! Rock beats Scissors");
     return "player";
-  } else if (humanChoiceLower === "r" && computerChoice === "p") {
+  } else if (humanChoice === "r" && computerChoice === "p") {
     console.log("You lose! Paper beats Rock");
     return "computer";
   } else {
@@ -40,10 +69,7 @@ const playRound = function (humanChoice, computerChoice) {
   }
 };
 
-const playGame = function () {
-  let humanScore = 0;
-  let computerScore = 0;
-
+const updateScore = function () {
   const result = playRound(getHumanChoice(), getComputerChoice());
   if (result === "player") {
     humanScore += 1;
@@ -60,8 +86,28 @@ const playGame = function () {
   }
 };
 
-const rock = document.querySelector("rock");
-const scissors = document.querySelector("scissors");
-const paper = document.querySelector("paper");
+const rock = document.querySelector(".rock");
+const scissors = document.querySelector(".scissors");
+const paper = document.querySelector(".paper");
+const playerImage = document.querySelector(".player-image");
+const computerImage = document.querySelector(".computer-image");
+const playerScoreDisplay = document.querySelector(".player-score");
+const computerScoreDisplay = document.querySelector(".computer-score");
 
-playGame();
+let humanScore = 0;
+let computerScore = 0;
+
+rock.addEventListener("click", (e) => {
+  const computerChoice = getComputerChoice();
+  playRound("r", computerChoice);
+});
+
+scissors.addEventListener("click", (e) => {
+  const computerChoice = getComputerChoice();
+  playRound("s", computerChoice);
+});
+
+paper.addEventListener("click", (e) => {
+  const computerChoice = getComputerChoice();
+  playRound("p", computerChoice);
+});
